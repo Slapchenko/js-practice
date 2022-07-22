d4a47397598614256fbc7692cfcf7d494a94b645
Serhii Lapchenko <lapchenko.ss@gmail.com>
2022-07-23 02:05:46 +0300

    solved 4 task

---
+++ js/hw-objects-destructuring-spread-rest.js
@@ -0,0 +1,99 @@
+// TODO 1. СОЗДАНИЕ ОБЪЕКТА
+/* Присвой переменной apartment объект описывающий квартиру со следующими характеристиками:
+
+imgUrl - фотография, значение "https://via.placeholder.com/640x480";
+descr - описание, значение "Spacious apartment in the city center";
+rating - рейтинг, значение 4;
+price - цена, значение 2153;
+tags - метаинформация, массив ["premium", "promoted", "top"]. */
+
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+//   tags: ['premium', 'promoted', 'top'],
+// };
+
+// console.log(apartment);
+
+// TODO 2. ВЛОЖЕННЫЕ СВОЙСТВА
+/* Дополни объект квартиры свойством owner, значением которого будет объект с информацией о владельце. Добавь ему следующие свойства:
+
+name - имя владельца, значение "Henry";
+phone - телефон, значение "982-126-1588";
+email - почта, значение "henry.carter@aptmail.com". */
+
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+//   tags: ['premium', 'promoted', 'top'],
+//   owner: {
+//     name: 'Henry',
+//     phone: '982-126-1588',
+//     email: 'henry.carter@aptmail.com',
+//   },
+// };
+
+// console.log(apartment);
+
+// TODO 3. ДОСТУП К СВОЙСТВАМ ЧЕРЕЗ ТОЧКУ
+/* Дополни код присвоив объявленным переменным выражения обращения к соответствующим свойствам обьекта apartment.
+
+aptRating - рейтинг;
+aptDescr - описание;
+aptPrice - цена;
+aptTags - теги. */
+
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+//   tags: ['premium', 'promoted', 'top'],
+// };
+
+// const aptRating = apartment.rating;
+// const aptDescr = apartment.descr;
+// const aptPrice = apartment.price;
+// const aptTags = apartment.tags;
+
+// console.log(apartment);
+// console.log(aptRating);
+// console.log(aptDescr);
+// console.log(aptPrice);
+// console.log(aptTags);
+
+// TODO 4. ДОСТУП К ВЛОЖЕННЫМ СВОЙСТВАМ
+/* Дополни код присвоив объявленным переменным выражения обращения к соответствующим свойствам обьекта apartment.
+
+ownerName - имя владельца;
+ownerPhone - телефон владельца;
+ownerEmail - почта владельца;
+numberOfTags - количество элементов массива в свойстве tags;
+firstTag - первый элемент массива в свойстве tags;
+lastTag - последний элемент массива в свойстве tags. */
+
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+//   tags: ['premium', 'promoted', 'top'],
+//   owner: {
+//     name: 'Henry',
+//     phone: '982-126-1588',
+//     email: 'henry.carter@aptmail.com',
+//   },
+// };
+
+// const ownerName = apartment.owner.name;
+// const ownerPhone = apartment.owner.phone;
+// const ownerEmail = apartment.owner.email;
+// const numberOfTags = apartment.tags.length;
+// const firstTag = apartment.tags[0];
+// const lastTag = apartment.tags[apartment.tags.length - 1];
+
+// console.log(apartment);
