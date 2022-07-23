4a6bbab97099f906520043f41f71f7253b66d7aa
Serhii Lapchenko <lapchenko.ss@gmail.com>
2022-07-23 14:30:00 +0300

    solved 7 task

--- js/hw-objects-destructuring-spread-rest.js
+++ js/hw-objects-destructuring-spread-rest.js
@@ -128,22 +128,50 @@ aptTags - теги. */
 имя владельца во вложенном свойстве name на "Henry Sibola";
 массив тегов в свойстве tags добавив в конец строку "trusted". */
 
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+//   tags: ['premium', 'promoted', 'top'],
+//   owner: {
+//     name: 'Henry',
+//     phone: '982-126-1588',
+//     email: 'henry.carter@aptmail.com',
+//   },
+// };
+
+// apartment.price = 5000;
+// apartment.rating = 4.7;
+// apartment.owner.name = 'Henry Sibola';
+// apartment.tags.push('trusted');
+
+// console.log(apartment);
+
+// TODO 7. ДОБАВЛЕНИЕ СВОЙСТВ
+/* Добавь объекту apartment несколько новых свойств:
+
+area - площадь в квадратных метрах, число 60;
+rooms - количество комнат, число 3;
+location - местоположение квартиры, обьект со следующими вложенными свойствами;
+country - страна, строка "Jamaica";
+city - город, строка "Kingston". */
+
 const apartment = {
   imgUrl: 'https://via.placeholder.com/640x480',
   descr: 'Spacious apartment in the city center',
-  rating: 4,
-  price: 2153,
-  tags: ['premium', 'promoted', 'top'],
+  rating: 4.7,
+  price: 5000,
+  tags: ['premium', 'promoted', 'top', 'trusted'],
   owner: {
-    name: 'Henry',
+    name: 'Henry Sibola',
     phone: '982-126-1588',
     email: 'henry.carter@aptmail.com',
   },
 };
 
-apartment.price = 5000;
-apartment.rating = 4.7;
-apartment.owner.name = 'Henry Sibola';
-apartment.tags.push('trusted');
+apartment.area = 60;
+apartment.rooms = 3;
+apartment.location = { country: 'Jamaica', city: 'Kingston' };
 
-console.log(apartment);
+// console.log(apartment);
