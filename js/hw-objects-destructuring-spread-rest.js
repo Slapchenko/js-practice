829ca9daa02f4ed2496e8fff4f5a0b6e099fbb9e
Serhii Lapchenko <lapchenko.ss@gmail.com>
2022-07-24 01:59:31 +0300

    solved 19 task

--- js/hw-objects-destructuring-spread-rest.js
+++ js/hw-objects-destructuring-spread-rest.js
@@ -370,3 +370,27 @@ city - город, строка "Kingston". */
 // }
 
 // console.log('Product price:', getProductPrice('Scanner'));
+
+// TODO 19. ЗАДАЧА. КОЛЛЕКЦИЯ ЗНАЧЕНИЙ СВОЙСТВА
+/* Напиши функцию getAllPropValues(propName) которая принимает один параметр propName - имя (ключ) свойства. Функция должна вернуть массив всех значений свойства с таким именем из каждого объекта в массиве products. Если в объектах нет свойства с таким именем, функция должна вернуть пустой массив. */
+
+const products = [
+  { name: 'Radar', price: 1300, quantity: 4 },
+  { name: 'Scanner', price: 2700, quantity: 3 },
+  { name: 'Droid', price: 400, quantity: 7 },
+  { name: 'Grip', price: 1200, quantity: 9 },
+];
+
+function getAllPropValues(propName) {
+  const propValue = [];
+
+  for (const product of products) {
+    if (product.hasOwnProperty(propName)) {
+      propValue.push(product[propName]);
+    }
+  }
+
+  return propValue;
+}
+
+console.log(getAllPropValues('name'));
