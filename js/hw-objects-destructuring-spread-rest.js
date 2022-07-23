7be3b40e70aef2687667e3f9f3ac2701b0ea6a6b
Serhii Lapchenko <lapchenko.ss@gmail.com>
2022-07-23 14:18:53 +0300

    solved 5-6 task

--- js/hw-objects-destructuring-spread-rest.js
+++ js/hw-objects-destructuring-spread-rest.js
@@ -97,3 +97,53 @@ lastTag - последний элемент массива в свойстве t
 // const lastTag = apartment.tags[apartment.tags.length - 1];
 
 // console.log(apartment);
+
+// TODO 5. ДОСТУП К СВОЙСТВАМ ЧЕРЕЗ КВАДРАТНЫЕ СКОБКИ
+/* Дополни код присвоив объявленным переменным выражения обращения к соответствующим свойствам обьекта apartment используя синтаксис «квадратных скобок».
+
+aptRating - рейтинг;
+aptDescr - описание;
+aptPrice - цена;
+aptTags - теги. */
+
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+//   tags: ['premium', 'promoted', 'top'],
+// };
+
+// const aptRating = apartment['rating'];
+// const aptDescr = apartment['descr'];
+// const aptPrice = apartment['price'];
+// const aptTags = apartment['tags'];
+
+// console.log(aptRating, aptDescr, aptPrice, aptTags);
+
+// TODO 6. ИЗМЕНЕНИЕ ЗНАЧЕНИЯ СВОЙСТВА
+/* Дополни код обновив значения свойств объекта apartment:
+цену в свойстве price на 5000;
+рейтинг квартиры в свойстве rating на 4.7;
+имя владельца во вложенном свойстве name на "Henry Sibola";
+массив тегов в свойстве tags добавив в конец строку "trusted". */
+
+const apartment = {
+  imgUrl: 'https://via.placeholder.com/640x480',
+  descr: 'Spacious apartment in the city center',
+  rating: 4,
+  price: 2153,
+  tags: ['premium', 'promoted', 'top'],
+  owner: {
+    name: 'Henry',
+    phone: '982-126-1588',
+    email: 'henry.carter@aptmail.com',
+  },
+};
+
+apartment.price = 5000;
+apartment.rating = 4.7;
+apartment.owner.name = 'Henry Sibola';
+apartment.tags.push('trusted');
+
+console.log(apartment);
