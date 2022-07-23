c4f70a6c9e347b2d121e25e3578697ccdf507a3d
Serhii Lapchenko <lapchenko.ss@gmail.com>
2022-07-23 15:34:05 +0300

    solved 12 task

--- js/hw-objects-destructuring-spread-rest.js
+++ js/hw-objects-destructuring-spread-rest.js
@@ -248,3 +248,20 @@ const credentials = {
 // }
 
 // console.log(apartment);
+
+// TODO 12. ЗАДАЧА. ПОДСЧЁТ СВОЙСТВ
+/* Напиши функцию countProps(object), которая считает и возвращает количество собственных свойств объекта в параметре object. Используй переменную propCount для хранения количества свойств объекта. */
+
+// function countProps(object) {
+//   let propCount = 0;
+
+//   for (const key in object) {
+//     if (object.hasOwnProperty(key)) {
+//       propCount += 1;
+//     }
+//   }
+
+//   return propCount;
+// }
+
+// console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 }));
