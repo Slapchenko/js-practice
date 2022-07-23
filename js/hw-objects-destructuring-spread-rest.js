7b3356a0cfde4c945fc57654b85350c7aa52a39b
Serhii Lapchenko <lapchenko.ss@gmail.com>
2022-07-23 15:14:23 +0300

    solved 11 task

--- js/hw-objects-destructuring-spread-rest.js
+++ js/hw-objects-destructuring-spread-rest.js
@@ -211,18 +211,40 @@ const credentials = {
 // TODO 10. ЦИКЛ FOR...IN
 /* Перебери объект apartment используя цикл for...in и запиши в массив keys все его ключи, а в массив values все значения его свойств. */
 
-const apartment = {
-  descr: 'Spacious apartment in the city center',
-  rating: 4,
-  price: 2153,
-};
-const keys = [];
-const values = [];
+// const apartment = {
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+// };
+// const keys = [];
+// const values = [];
 
-for (const key in apartment) {
-  keys.push(key);
-  values.push(apartment[key]);
-}
+// for (const key in apartment) {
+//   keys.push(key);
+//   values.push(apartment[key]);
+// }
 
-console.log(keys);
-console.log(values);
+// console.log(keys);
+// console.log(values);
+
+// TODO 11. МЕТОД HASOWNPROPERTY()
+/* Выполни рефакторинг решения предыдущего задания добавив в цикл for...in проверку на собственное свойство. */
+
+// const keys = [];
+// const values = [];
+// const advert = {
+//   service: 'apt',
+// };
+// const apartment = Object.create(advert);
+// apartment.descr = 'Spacious apartment in the city center';
+// apartment.rating = 4;
+// apartment.price = 2153;
+
+// for (const key in apartment) {
+//   if (apartment.hasOwnProperty(key)) {
+//     keys.push(key);
+//     values.push(apartment[key]);
+//   }
+// }
+
+// console.log(apartment);
