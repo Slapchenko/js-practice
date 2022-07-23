162272e377a9b1356a5b8c19d243dc00559a4856
Serhii Lapchenko <lapchenko.ss@gmail.com>
2022-07-23 16:05:23 +0300

    solved 13 task

--- js/hw-objects-destructuring-spread-rest.js
+++ js/hw-objects-destructuring-spread-rest.js
@@ -265,3 +265,20 @@ const credentials = {
 // }
 
 // console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 }));
+
+// TODO 13. МЕТОД OBJECT.KEYS()
+/* Перебери объект apartment используя метод Object.keys() и цикл for...of. Запиши в переменную keys массив ключей собственных свойств объекта apartment, и добавь в массив values все значения его свойств. */
+
+// const apartment = {
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+// };
+// const values = [];
+// const keys = Object.keys(apartment);
+
+// for (const key of keys) {
+//   values.push(apartment[key]);
+// }
+
+// console.log(values);
