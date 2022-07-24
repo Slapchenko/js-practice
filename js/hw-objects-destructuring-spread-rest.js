57026505d849186f56c79cc7364d75daf4a750f0
Serhii Lapchenko <lapchenko.ss@gmail.com>
2022-07-24 14:15:12 +0300

    solved 22 task

--- js/hw-objects-destructuring-spread-rest.js
+++ js/hw-objects-destructuring-spread-rest.js
@@ -422,13 +422,33 @@ city - город, строка "Kingston". */
 // TODO 21. ДЕСТРУКТУРИЗАЦИЯ ОБЪЕКТОВ
 /* Пришел трёхдневный прогноз максимальных температур и мы считаем среднюю температуру за три дня (meanTemperature). Замени объявления переменных yesterday, today и tomorrow одной операцией деструктуризации свойств объекта highTemperatures. */
 
+// const highTemperatures = {
+//   yesterday: 28,
+//   today: 26,
+//   tomorrow: 33,
+// };
+
+// const { yesterday, today, tomorrow } = highTemperatures;
+
+// const meanTemperature = (yesterday + today + tomorrow) / 3;
+
+// console.log(meanTemperature);
+
+// TODO 22. ЗНАЧЕНИЯ ПО УМОЛЧАНИЮ
+/* В прогнозе максимальных температур также может быть необязательное свойство icon - иконка погоды. Замени объявления переменных yesterday, today, tomorrow и icon одной операцией деструктуризации свойств объекта highTemperatures. Задай значение по умолчанию для icon - строку "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg". */
+
 const highTemperatures = {
   yesterday: 28,
   today: 26,
   tomorrow: 33,
 };
 
-const { yesterday, today, tomorrow } = highTemperatures;
+const {
+  yesterday,
+  today,
+  tomorrow,
+  icon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg',
+} = highTemperatures;
 
 const meanTemperature = (yesterday + today + tomorrow) / 3;
 
