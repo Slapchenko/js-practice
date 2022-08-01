20d1f313904a92c451bf3999b1cc1c3612621e70
Serhii Lapchenko <lapchenko.ss@gmail.com>
2022-08-01 15:43:50 +0300

    Function refactoring - filtering an array of numbers

--- js/hw-callback-arrows-methods.js
+++ js/hw-callback-arrows-methods.js
@@ -110,3 +110,23 @@
 // }
 
 // console.log(calculateTotalPrice([12, 85, 37, 4])); //138
+
+// TODO 6. ЗАДАЧА. ФИЛЬТРАЦИЯ МАССИВА ЧИСЕЛ
+/* Функция filterArray(numbers, value) принимает массив чисел numbers и возвращает новый массив, в котором будут только те элементы оригинального массива, которые больше чем значение параметра value.
+
+Выполни рефакторинг функции так, чтобы вместо цикла for она использовала метод forEach.
+ */
+
+function filterArray(numbers, value) {
+  const filteredNumbers = [];
+
+  numbers.forEach(number => {
+    if (number > value) {
+      filteredNumbers.push(number);
+    }
+  });
+
+  return filteredNumbers;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
