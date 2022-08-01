0951a7fc569e5951d9d63863b0031b24e77e62b2
Serhii Lapchenko <lapchenko.ss@gmail.com>
2022-08-01 17:07:46 +0300

    A task. Filtering an array of numbers

--- js/hw-callback-arrows-methods.js
+++ js/hw-callback-arrows-methods.js
@@ -167,12 +167,29 @@
 // TODO 10. СТРЕЛОЧНЫЕ ФУНКЦИИ КАК КОЛЛБЕКИ
 /* Выполни рефакторинг функции calculateTotalPrice(orderedItems) заменив её объявление на стрелочную функцию. Замени коллбек-функцию передаваемую в метод forEach() на стрелочную функцию. */
 
-const calculateTotalPrice = orderedItems => {
-  let totalPrice = 0;
+// const calculateTotalPrice = orderedItems => {
+//   let totalPrice = 0;
 
-  orderedItems.forEach(item => (totalPrice += item));
+//   orderedItems.forEach(item => (totalPrice += item));
 
-  return totalPrice;
+//   return totalPrice;
+// };
+
+// console.log(calculateTotalPrice([12, 85, 37, 4])); // 138
+
+//  TODO 11. ЗАДАЧА. ФИЛЬТРАЦИЯ МАССИВА ЧИСЕЛ 2.0
+/* Замени объявление функции filterArray() и коллбек для метода forEach() на стрелочные функции. */
+
+const filterArray = (numbers, value) => {
+  const filteredNumbers = [];
+
+  numbers.forEach(number => {
+    if (number > value) {
+      filteredNumbers.push(number);
+    }
+  });
+
+  return filteredNumbers;
 };
 
-console.log(calculateTotalPrice([12, 85, 37, 4])); // 138
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
