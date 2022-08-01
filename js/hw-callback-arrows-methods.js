de9b337eaf108fc9a260c08dbcdfcd988f242858
Serhii Lapchenko <lapchenko.ss@gmail.com>
2022-08-01 11:46:14 +0300

    declares a function as a value

---
+++ js/hw-callback-arrows-methods.js
@@ -0,0 +1,12 @@
+//TODO 1. ФУНКЦИЯ КАК ЗНАЧЕНИЕ
+/* Дополни код так, чтобы в переменной result был результат выполнения функции makePizza, а в переменной pointer была ссылка на функцию makePizza. */
+
+// function makePizza() {
+//   return 'Your pizza is being prepared, please wait.';
+// }
+
+// const result = makePizza();
+// const pointer = makePizza;
+
+// console.log(result);
+// console.log(pointer);
