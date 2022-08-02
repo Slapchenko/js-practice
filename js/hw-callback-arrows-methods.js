3f8b9a29500613a61cee0d7b356d93094f68c017
Serhii Lapchenko <lapchenko.ss@gmail.com>
2022-08-02 14:22:27 +0300

    A TASK. SORT BY NUMBER OF FRIENDS

--- js/hw-callback-arrows-methods.js
+++ js/hw-callback-arrows-methods.js
@@ -1613,6 +1613,80 @@
 // TODO 42. ЗАДАЧА. СОРТИРОВКА ПО БАЛАНСУ
 /* Дополни функцию sortByAscendingBalance(users) так, чтобы она возвращала массив пользователей отсортированный по возрастанию их баланса (свойство balance). */
 
+// const users = [
+//   {
+//     name: 'Moore Hensley',
+//     email: 'moorehensley@indexia.com',
+//     eyeColor: 'blue',
+//     friends: ['Sharron Pace'],
+//     isActive: false,
+//     balance: 2811,
+//     gender: 'male',
+//   },
+//   {
+//     name: 'Sharlene Bush',
+//     email: 'sharlenebush@tubesys.com',
+//     eyeColor: 'blue',
+//     friends: ['Briana Decker', 'Sharron Pace'],
+//     isActive: true,
+//     balance: 3821,
+//     gender: 'female',
+//   },
+//   {
+//     name: 'Ross Vazquez',
+//     email: 'rossvazquez@xinware.com',
+//     eyeColor: 'green',
+//     friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+//     isActive: false,
+//     balance: 3793,
+//     gender: 'male',
+//   },
+//   {
+//     name: 'Elma Head',
+//     email: 'elmahead@omatom.com',
+//     eyeColor: 'green',
+//     friends: ['Goldie Gentry', 'Aisha Tran'],
+//     isActive: true,
+//     balance: 2278,
+//     gender: 'female',
+//   },
+//   {
+//     name: 'Carey Barr',
+//     email: 'careybarr@nurali.com',
+//     eyeColor: 'blue',
+//     friends: ['Jordan Sampson', 'Eddie Strong'],
+//     isActive: true,
+//     balance: 3951,
+//     gender: 'male',
+//   },
+//   {
+//     name: 'Blackburn Dotson',
+//     email: 'blackburndotson@furnigeer.com',
+//     eyeColor: 'brown',
+//     friends: ['Jacklyn Lucas', 'Linda Chapman'],
+//     isActive: false,
+//     balance: 1498,
+//     gender: 'male',
+//   },
+//   {
+//     name: 'Sheree Anthony',
+//     email: 'shereeanthony@kog.com',
+//     eyeColor: 'brown',
+//     friends: ['Goldie Gentry', 'Briana Decker'],
+//     isActive: true,
+//     balance: 2764,
+//     gender: 'female',
+//   },
+// ];
+
+// const sortByAscendingBalance = users => [...users].sort((a, b) => a.balance - b.balance);
+
+// console.table(sortByAscendingBalance(users));
+
+// TODO 43. ЗАДАЧА. СОРТИРОВКА ПО КОЛИЧЕСТВУ ДРУЗЕЙ
+/* Дополни функцию sortByDescendingFriendCount(users) так, чтобы она возвращала массив пользователей отсортированный по убыванию количества их друзей (свойство friends).
+ */
+
 const users = [
   {
     name: 'Moore Hensley',
@@ -1679,6 +1753,7 @@ const users = [
   },
 ];
 
-const sortByAscendingBalance = users => [...users].sort((a, b) => a.balance - b.balance);
+const sortByDescendingFriendCount = users =>
+  [...users].sort((a, b) => b.friends.length - a.friends.length);
 
-console.table(sortByAscendingBalance(users));
+console.table(sortByDescendingFriendCount(users));
