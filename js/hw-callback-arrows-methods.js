1285008a486732ffc9d086d9470a6c032c9904a5
Serhii Lapchenko <lapchenko.ss@gmail.com>
2022-08-02 12:56:17 +0300

    REDUCE() METHOD AND OBJECT ARRAY

--- js/hw-callback-arrows-methods.js
+++ js/hw-callback-arrows-methods.js
@@ -1351,16 +1351,33 @@
 // TODO 34. МЕТОД REDUCE()
 /* Игровому сервису необходим функционал подсчёта среднего времени проведённого в играх. Дополни код так, чтобы в переменной totalPlayTime получилось общее игровое время из массива playtimes. */
 
-const players = {
-  mango: 1270,
-  poly: 468,
-  ajax: 710,
-  kiwi: 244,
-};
-const playtimes = Object.values(players); // [1270, 468, 710, 244]
+// const players = {
+//   mango: 1270,
+//   poly: 468,
+//   ajax: 710,
+//   kiwi: 244,
+// };
+// const playtimes = Object.values(players); // [1270, 468, 710, 244]
 
-const totalPlayTime = playtimes.reduce((previousValue, number) => previousValue + number, 0);
+// const totalPlayTime = playtimes.reduce((previousValue, number) => previousValue + number, 0);
 
-const averagePlayTime = totalPlayTime / playtimes.length;
+// const averagePlayTime = totalPlayTime / playtimes.length;
 
-console.log(totalPlayTime);
+// console.log(totalPlayTime);
+
+// TODO 35. МЕТОД REDUCE() И МАССИВ ОБЪЕКТОВ
+/* Нашему сервису необходимо рассчитать среднее время проведённое в одной игре для каждого игрока, и получить общую сумму этих времён. Рассчитать время для каждого из игроков, можно разделив его время (свойство playtime) на количество игр (свойство gamesPlayed). */
+
+// const players = [
+//   { name: 'Mango', playtime: 1270, gamesPlayed: 4 },
+//   { name: 'Poly', playtime: 469, gamesPlayed: 2 },
+//   { name: 'Ajax', playtime: 690, gamesPlayed: 3 },
+//   { name: 'Kiwi', playtime: 241, gamesPlayed: 1 },
+// ];
+
+// const totalAveragePlaytimePerGame = players.reduce(
+//   (previousValue, number) => previousValue + number.playtime / number.gamesPlayed,
+//   0
+// );
+
+// console.log(totalAveragePlaytimePerGame);
