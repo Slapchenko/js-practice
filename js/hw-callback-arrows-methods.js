da4a056c148b09b73169d0c4d4342e1dae024cdf
Serhii Lapchenko <lapchenko.ss@gmail.com>
2022-08-02 13:55:12 +0300

    ROW SORT ORDER

--- js/hw-callback-arrows-methods.js
+++ js/hw-callback-arrows-methods.js
@@ -1543,10 +1543,27 @@
 // TODO 39. СВОЙ ПОРЯДОК СОРТИРОВКИ ЧИСЕЛ
 /*  Онлайн бибилиотеке необходимо отображать книги сортированные по дате издания, по её возрастанию или убыванию. Дополни код так, чтобы в переменной ascendingReleaseDates получилась отсортированная по возрастанию копия массива releaseDates, а в переменной descendingReleaseDates копия отсортированная по убыванию.*/
 
-const releaseDates = [2016, 1967, 2008, 1984, 1973, 2012, 1997];
+// const releaseDates = [2016, 1967, 2008, 1984, 1973, 2012, 1997];
 
-const ascendingReleaseDates = [...releaseDates].sort((a, b) => a - b);
+// const ascendingReleaseDates = [...releaseDates].sort((a, b) => a - b);
 
-const descendingReleaseDates = [...releaseDates].sort((a, b) => b - a);
+// const descendingReleaseDates = [...releaseDates].sort((a, b) => b - a);
 
-console.log(`Ascending:`, ascendingReleaseDates, `Ascending:`, descendingReleaseDates);
+// console.log(`Ascending:`, ascendingReleaseDates, `Ascending:`, descendingReleaseDates);
+
+// TODO 40. СВОЙ ПОРЯДОК СОРТИРОВКИ СТРОК
+/* Онлайн бибилиотеке необходимо отображать книги отсортированные по автору, в алфавитном и обратном алфавитном порядке. Дополни код так, чтобы в переменной authorsInAlphabetOrder получилась отсортированная по алфавиту копия массива authors, а в переменной authorsInReversedOrder копия отсортированная в обратном алфавитном порядке. */
+
+const authors = [
+  'Tanith Lee',
+  'Bernard Cornwell',
+  'Robert Sheckley',
+  'Fyodor Dostoevsky',
+  'Howard Lovecraft',
+];
+
+const authorsInAlphabetOrder = [...authors].sort((a, b) => a.localeCompare(b));
+
+const authorsInReversedOrder = [...authors].sort((a, b) => b.localeCompare(a));
+
+console.log(`InAlphabe:`, authorsInAlphabetOrder, `InReverse:`, authorsInReversedOrder);
