76ba3fd0082bb1020b0156b7156ac6d40da0ea19
Serhii Lapchenko <lapchenko.ss@gmail.com>
2022-08-02 10:42:11 +0300

    A TASK. ARE THERE ACTIVE USERS

--- js/hw-callback-arrows-methods.js
+++ js/hw-callback-arrows-methods.js
@@ -1262,15 +1262,88 @@
 5. В переменной anyElementInThirdIsEven был результат проверки наличия чётных элементов в массиве thirdArray.
 6. В переменной anyElementInThirdIsOdd был результат проверки наличия нечётных элементов в массиве thirdArray. */
 
-const firstArray = [26, 94, 36, 18];
-const secondArray = [17, 61, 23];
-const thirdArray = [17, 26, 94, 61, 36, 23, 18];
+// const firstArray = [26, 94, 36, 18];
+// const secondArray = [17, 61, 23];
+// const thirdArray = [17, 26, 94, 61, 36, 23, 18];
 
-const anyElementInFirstIsEven = firstArray.some(element => element % 2 === 0);
-const anyElementInFirstIsOdd = firstArray.some(element => element % 2 !== 0);
+// const anyElementInFirstIsEven = firstArray.some(element => element % 2 === 0);
+// const anyElementInFirstIsOdd = firstArray.some(element => element % 2 !== 0);
 
-const anyElementInSecondIsEven = secondArray.some(element => element % 2 === 0);
-const anyElementInSecondIsOdd = secondArray.some(element => element % 2 !== 0);
+// const anyElementInSecondIsEven = secondArray.some(element => element % 2 === 0);
+// const anyElementInSecondIsOdd = secondArray.some(element => element % 2 !== 0);
 
-const anyElementInThirdIsEven = thirdArray.some(element => element % 2 === 0);
-const anyElementInThirdIsOdd = thirdArray.some(element => element % 2 !== 0);
+// const anyElementInThirdIsEven = thirdArray.some(element => element % 2 === 0);
+// const anyElementInThirdIsOdd = thirdArray.some(element => element % 2 !== 0);
+
+// TODO 33. ЗАДАЧА. ЕСТЬ ЛИ АКТИВНЫЕ ПОЛЬЗОВАТЕЛИ
+/* Дополни функцию isAnyUserActive(users) так, чтобы она проверяла наличие активных пользователей (свойство isActive) и возвращала true или false. */
+
+const users = [
+  {
+    name: 'Moore Hensley',
+    email: 'moorehensley@indexia.com',
+    eyeColor: 'blue',
+    friends: ['Sharron Pace'],
+    isActive: false,
+    balance: 2811,
+    gender: 'male',
+  },
+  {
+    name: 'Sharlene Bush',
+    email: 'sharlenebush@tubesys.com',
+    eyeColor: 'blue',
+    friends: ['Briana Decker', 'Sharron Pace'],
+    isActive: true,
+    balance: 3821,
+    gender: 'female',
+  },
+  {
+    name: 'Ross Vazquez',
+    email: 'rossvazquez@xinware.com',
+    eyeColor: 'green',
+    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+    isActive: false,
+    balance: 3793,
+    gender: 'male',
+  },
+  {
+    name: 'Elma Head',
+    email: 'elmahead@omatom.com',
+    eyeColor: 'green',
+    friends: ['Goldie Gentry', 'Aisha Tran'],
+    isActive: true,
+    balance: 2278,
+    gender: 'female',
+  },
+  {
+    name: 'Carey Barr',
+    email: 'careybarr@nurali.com',
+    eyeColor: 'blue',
+    friends: ['Jordan Sampson', 'Eddie Strong'],
+    isActive: true,
+    balance: 3951,
+    gender: 'male',
+  },
+  {
+    name: 'Blackburn Dotson',
+    email: 'blackburndotson@furnigeer.com',
+    eyeColor: 'brown',
+    friends: ['Jacklyn Lucas', 'Linda Chapman'],
+    isActive: false,
+    balance: 1498,
+    gender: 'male',
+  },
+  {
+    name: 'Sheree Anthony',
+    email: 'shereeanthony@kog.com',
+    eyeColor: 'brown',
+    friends: ['Goldie Gentry', 'Briana Decker'],
+    isActive: true,
+    balance: 2764,
+    gender: 'female',
+  },
+];
+
+const isAnyUserActive = users => users.some(user => user.isActive);
+
+console.log(isAnyUserActive(users));
