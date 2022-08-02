1442be747cd7e16cdfb88d26bfea21d1dea3b7ed
Serhii Lapchenko <lapchenko.ss@gmail.com>
2022-08-02 13:46:03 +0300

    YOUR OWN SORTING ORDER

--- js/hw-callback-arrows-methods.js
+++ js/hw-callback-arrows-methods.js
@@ -1531,11 +1531,22 @@
 // TODO 38. МЕТОД SORT()
 /* Дополни код так, чтобы в переменной ascendingReleaseDates получилась отсортированная по возрастанию копия массива releaseDates, а в переменной alphabeticalAuthors копия массива имён авторов authors отсортированная в по алфавиту. */
 
+// const releaseDates = [2016, 1967, 2008, 1984, 1973, 2012, 1997];
+// const authors = ['Tanith Lee', 'Bernard Cornwell', 'Robert Sheckley', 'Fyodor Dostoevsky'];
+
+// const ascendingReleaseDates = [...releaseDates].sort();
+
+// const alphabeticalAuthors = [...authors].sort();
+
+// console.log(ascendingReleaseDates, alphabeticalAuthors);
+
+// TODO 39. СВОЙ ПОРЯДОК СОРТИРОВКИ ЧИСЕЛ
+/*  Онлайн бибилиотеке необходимо отображать книги сортированные по дате издания, по её возрастанию или убыванию. Дополни код так, чтобы в переменной ascendingReleaseDates получилась отсортированная по возрастанию копия массива releaseDates, а в переменной descendingReleaseDates копия отсортированная по убыванию.*/
+
 const releaseDates = [2016, 1967, 2008, 1984, 1973, 2012, 1997];
-const authors = ['Tanith Lee', 'Bernard Cornwell', 'Robert Sheckley', 'Fyodor Dostoevsky'];
 
-const ascendingReleaseDates = [...releaseDates].sort();
+const ascendingReleaseDates = [...releaseDates].sort((a, b) => a - b);
 
-const alphabeticalAuthors = [...authors].sort();
+const descendingReleaseDates = [...releaseDates].sort((a, b) => b - a);
 
-console.log(ascendingReleaseDates, alphabeticalAuthors);
+console.log(`Ascending:`, ascendingReleaseDates, `Ascending:`, descendingReleaseDates);
