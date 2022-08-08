cec0812bba09fc1665880b95ceb1e773ba6889cb
Serhii Lapchenko <lapchenko.ss@gmail.com>
2022-08-08 23:20:22 +0300

    CLASS CONSTRUCTOR

--- js/hw-this-prototypes-classes.js
+++ js/hw-this-prototypes-classes.js
@@ -121,4 +121,23 @@
 // TODO 6. ОБЪЯВЛЕНИЕ КЛАССА
 /* Используя ключевое слово class объяви класс Car с пустым телом. */
 
-class Car {}
+// class Car {}
+
+// TODO 7. КОНСТРУКТОР КЛАССА
+/* Добавь классу Car метод constructor который принимает три параметра:
+
+brand - марка автомобиля.
+model - модель автомобиля.
+price - цена автомобиля.
+Класс Car должен создавать объект с одноимёнными свойствами brand, model и price, значениями которых должны быть переданные аргументы во время её вызова с оператором new. */
+
+class Car {
+  constructor(brand, model, price) {
+    this.brand = brand;
+    this.model = model;
+    this.price = price;
+  }
+}
+
+const bmw = new Car('bmw', 'x5', 60000);
+console.log(bmw);
