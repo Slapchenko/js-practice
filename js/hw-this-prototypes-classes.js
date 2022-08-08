c637bbe5fe49b5f71319565c882a8e561699a80f
Serhii Lapchenko <lapchenko.ss@gmail.com>
2022-08-08 22:43:57 +0300

    TASK: USER ACCOUNT

--- js/hw-this-prototypes-classes.js
+++ js/hw-this-prototypes-classes.js
@@ -1,22 +1,57 @@
 // TODO 1. КОНТЕКСТ ВЫЗОВА ФУНКЦИИ
 /* Выполни рефакторинг методов объекта pizzaPalace, расставив отсутствующие this в местах обращения к свойствам и методам объекта. */
 
-const pizzaPalace = {
-  pizzas: ['Supercheese', 'Smoked', 'Four meats'],
+// const pizzaPalace = {
+//   pizzas: ['Supercheese', 'Smoked', 'Four meats'],
 
-  checkPizza(pizzaName) {
-    return this.pizzas.includes(pizzaName);
+//   checkPizza(pizzaName) {
+//     return this.pizzas.includes(pizzaName);
+//   },
+//   order(pizzaName) {
+//     const isPizzaAvailable = this.checkPizza(pizzaName);
+
+//     if (!isPizzaAvailable) {
+//       return `Sorry, there is no pizza named «${pizzaName}»`;
+//     }
+
+//     return `Order accepted, preparing «${pizzaName}» pizza`;
+//   },
+// };
+
+// console.log(pizzaPalace.order('Smoked'));
+// // "Order accepted, preparing «Smoked» pizza"
+
+// TODO 2. ЗАДАЧА: АККАУНТ ПОЛЬЗОВАТЕЛЯ
+/* Перед увольнением разработчик сломал исходный код управления аккаунтами пользователей нашего сервиса доставки еды. Выполни рефакторинг методов объекта customer, расставив отсутствующие this при обращении к свойствам объекта.
+
+После объявления объекта мы добавили вызовы методов в той последовательности, в которой твой код будут проверять тесты. Пожалуйста ничего там не меняй. */
+
+const customer = {
+  username: 'Mango',
+  balance: 24000,
+  discount: 0.1,
+  orders: ['Burger', 'Pizza', 'Salad'],
+  // Change code below this line
+  getBalance() {
+    return this.balance;
   },
-  order(pizzaName) {
-    const isPizzaAvailable = this.checkPizza(pizzaName);
-
-    if (!isPizzaAvailable) {
-      return `Sorry, there is no pizza named «${pizzaName}»`;
-    }
-
-    return `Order accepted, preparing «${pizzaName}» pizza`;
+  getDiscount() {
+    return this.discount;
+  },
+  setDiscount(value) {
+    this.discount = value;
+  },
+  getOrders() {
+    return this.orders;
+  },
+  addOrder(cost, order) {
+    this.balance -= cost - cost * this.discount;
+    this.orders.push(order);
   },
 };
 
-console.log(pizzaPalace.order('Smoked'));
-// "Order accepted, preparing «Smoked» pizza"
+customer.setDiscount(0.15);
+console.log(customer.getDiscount()); // 0.15
+customer.addOrder(5000, 'Steak');
+console.log(customer.getBalance()); // 19750
+console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
