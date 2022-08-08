c4602f3795580926260ef433671330d6ddb22725
Serhii Lapchenko <lapchenko.ss@gmail.com>
2022-08-08 22:49:56 +0300

    TASK: ORDER HISTORY

--- js/hw-this-prototypes-classes.js
+++ js/hw-this-prototypes-classes.js
@@ -26,32 +26,60 @@
 
 После объявления объекта мы добавили вызовы методов в той последовательности, в которой твой код будут проверять тесты. Пожалуйста ничего там не меняй. */
 
-const customer = {
-  username: 'Mango',
-  balance: 24000,
-  discount: 0.1,
-  orders: ['Burger', 'Pizza', 'Salad'],
-  // Change code below this line
-  getBalance() {
-    return this.balance;
+// const customer = {
+//   username: 'Mango',
+//   balance: 24000,
+//   discount: 0.1,
+//   orders: ['Burger', 'Pizza', 'Salad'],
+//   // Change code below this line
+//   getBalance() {
+//     return this.balance;
+//   },
+//   getDiscount() {
+//     return this.discount;
+//   },
+//   setDiscount(value) {
+//     this.discount = value;
+//   },
+//   getOrders() {
+//     return this.orders;
+//   },
+//   addOrder(cost, order) {
+//     this.balance -= cost - cost * this.discount;
+//     this.orders.push(order);
+//   },
+// };
+
+// customer.setDiscount(0.15);
+// console.log(customer.getDiscount()); // 0.15
+// customer.addOrder(5000, 'Steak');
+// console.log(customer.getBalance()); // 19750
+// console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+
+// TODO 3. ЗАДАЧА: ИСТОРИЯ ЗАКАЗОВ
+/* Тестировщики нашли баги в коде сервиса хранения истории заказов еды. Тебе необходимо исправить их, правильно расставив this в методах объекта historyService, чтобы методы начали работать правильно. */
+
+const historyService = {
+  orders: [
+    { email: 'jacob@hotmail.com', dish: 'Burrito' },
+    { email: 'solomon@topmail.net', dish: 'Burger' },
+    { email: 'artemis@coldmail.net', dish: 'Pizza' },
+    { email: 'solomon@topmail.net', dish: 'Apple pie' },
+    { email: 'jacob@hotmail.com', dish: 'Taco' },
+  ],
+
+  getOrdersLog() {
+    return this.orders.map(order => `email: ${order.email} dish: ${order.dish}`).join(' - ');
   },
-  getDiscount() {
-    return this.discount;
+  getEmails() {
+    const emails = this.orders.map(order => order.email);
+    const uniqueEmails = new Set(emails);
+    return [...uniqueEmails];
   },
-  setDiscount(value) {
-    this.discount = value;
-  },
-  getOrders() {
-    return this.orders;
-  },
-  addOrder(cost, order) {
-    this.balance -= cost - cost * this.discount;
-    this.orders.push(order);
+  getOrdersByEmail(email) {
+    return this.orders.filter(order => order.email === email);
   },
 };
 
-customer.setDiscount(0.15);
-console.log(customer.getDiscount()); // 0.15
-customer.addOrder(5000, 'Steak');
-console.log(customer.getBalance()); // 19750
-console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+console.log(historyService.getOrdersByEmail('solomon@topmail.net'));
+// [{ email: "solomon@topmail.net", dish: "Burger" }, { email: "solomon@topmail.net", dish: "Apple pie" }]
