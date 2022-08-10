c64c8f892328ad18bc3446eb2cccb81ffa6c843a
Serhii Lapchenko <lapchenko.ss@gmail.com>
2022-08-10 22:43:37 +0300

    completed task 3 - codewars

--- js/training-codewars.js
+++ js/training-codewars.js
@@ -31,22 +31,47 @@ XO("ooxXm") => true
 XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
 XO("zzoo") => false */
 
-function XO(str) {
-  const x = str
-    .toLowerCase()
-    .split('')
-    .filter(value => value === 'x');
+// function XO(str) {
+//   const x = str
+//     .toLowerCase()
+//     .split('')
+//     .filter(value => value === 'x');
 
-  const o = str
-    .toLowerCase()
-    .split('')
-    .filter(value => value === 'o');
+//   const o = str
+//     .toLowerCase()
+//     .split('')
+//     .filter(value => value === 'o');
 
-  return x.length === o.length;
-}
+//   return x.length === o.length;
+// }
 
-console.log(XO('ooxx'));
-console.log(XO('xooxx'));
-console.log(XO('ooxXm'));
-console.log(XO('zpzpzpp'));
-console.log(XO('zzoo'));
+// console.log(XO('ooxx'));
+// console.log(XO('xooxx'));
+// console.log(XO('ooxXm'));
+// console.log(XO('zpzpzpp'));
+// console.log(XO('zzoo'));
+
+// TODO Task #3
+/* Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each. If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+
+Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't. */
+
+// * v1
+// function lovefunc(flower1, flower2) {
+//   const isflower1Even = flower1 % 2 === 0;
+//   const isflower2Even = flower2 % 2 === 0;
+//   if (isflower1Even !== isflower2Even) {
+//     return true;
+//   }
+//   return false;
+// }
+
+// * v2
+// function lovefunc(flower1, flower2) {
+//   return flower1 % 2 !== flower2 % 2;
+// }
+
+// console.log(lovefunc(1, 4)); // true
+// console.log(lovefunc(2, 2)); // false
+// console.log(lovefunc(0, 1)); // true
+// console.log(lovefunc(0, 0)); // false
