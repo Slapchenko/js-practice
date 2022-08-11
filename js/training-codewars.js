8997c8a2a06976c11691d615a7658f2da28778d4
Serhii Lapchenko <lapchenko.ss@gmail.com>
2022-08-11 17:54:08 +0300

    completed task 7 - codewars

--- js/training-codewars.js
+++ js/training-codewars.js
@@ -140,11 +140,26 @@ Note: Empty arrays should return 0. */
 // }
 
 // * v2
-function find_average(array) {
-  return array.length > 0 ? array.reduce((acc, number) => acc + number) / array.length : 0;
-}
+// function find_average(array) {
+//   return array.length > 0 ? array.reduce((acc, number) => acc + number) / array.length : 0;
+// }
 
-console.log(find_average([1, 1, 1])); // 1
-console.log(find_average([1, 2, 3])); // 2
-console.log(find_average([1, 2, 3, 4])); // 2.5
-console.log(find_average([])); // 0
+// console.log(find_average([1, 1, 1])); // 1
+// console.log(find_average([1, 2, 3])); // 2
+// console.log(find_average([1, 2, 3, 4])); // 2.5
+// console.log(find_average([])); // 0
+
+// TODO Task #7 Square(n) Sum
+/* Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9. */
+
+// function squareSum(numbers) {
+//   return numbers.length > 0
+//     ? numbers.map(element => Math.pow(element, 2)).reduce((acc, number) => acc + number)
+//     : 0;
+// }
+
+// console.log(squareSum([1, 2])); // 5
+// console.log(squareSum([0, 3, 4, 5])); // 50
+// console.log(squareSum([])); // 0
