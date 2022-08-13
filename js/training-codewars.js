228a8ab4b35595c096b6f4b7a2b1bb470e75241d
Serhii Lapchenko <lapchenko.ss@gmail.com>
2022-08-13 15:36:47 +0300

    completed task 8 - codewars

--- js/training-codewars.js
+++ js/training-codewars.js
@@ -163,3 +163,34 @@ For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9. */
 // console.log(squareSum([1, 2])); // 5
 // console.log(squareSum([0, 3, 4, 5])); // 50
 // console.log(squareSum([])); // 0
+
+// TODO Task #8 Complementary DNA
+/* Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
+
+If you want to know more: http://en.wikipedia.org/wiki/DNA
+
+In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
+
+Example: (input --> output)
+
+"ATTGC" --> "TAACG"
+"GTAT" --> "CATA" */
+
+function DNAStrand(dna) {
+  const complementarySide = [...dna]
+    .map((symbol, index, array) => {
+      if (symbol === 'A') return (array[index] = 'T');
+      if (symbol === 'T') return (array[index] = 'A');
+      if (symbol === 'C') return (array[index] = 'G');
+      if (symbol === 'G') return (array[index] = 'C');
+    })
+    .join('');
+
+  return `${complementarySide}, String ${dna} is`;
+}
+
+console.log(DNAStrand('AAAA')); // TTTT', 'String AAAA is');
+console.log(DNAStrand('ATTGC')); //  'TAACG', 'String ATTGC is');
+console.log(DNAStrand('GTAT')); // CATA', 'String GTAT is');
