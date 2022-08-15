0560ef69c1d11b2dba4c5969967a1d1e7c1ed85a
Serhii Lapchenko <lapchenko.ss@gmail.com>
2022-08-15 14:25:15 +0300

    completed task 12 - codewars

--- js/training-codewars.js
+++ js/training-codewars.js
@@ -295,3 +295,44 @@ You can assume, for the purpose of this kata, that the supplied array will not b
 // const highestNumber = new SmallestIntegerFinder();
 
 // console.log(highestNumber.findSmallestInt([78, 56, 232, 12, 8])); // 8
+
+// TODO Task #12 Basic Mathematical Operations
+/* Your task is to create a function that does four basic mathematical operations.
+
+The function should take three arguments - operation(string/char), value1(number), value2(number).
+The function should return result of numbers after applying the chosen operation.
+
+Examples(Operator, value1, value2) --> output
+('+', 4, 7) --> 11
+('-', 15, 18) --> -3
+('*', 5, 5) --> 25
+('/', 49, 7) --> 7 */
+
+// function basicOp(operation, value1, value2) {
+//   let result = 0;
+
+//   switch (operation) {
+//     case '+':
+//       result = value1 + value2;
+//       break;
+
+//     case '-':
+//       result = value1 - value2;
+//       break;
+
+//     case '*':
+//       result = value1 * value2;
+//       break;
+
+//     case '/':
+//       result = value1 / value2;
+//       break;
+//   }
+
+//   return result;
+// }
+
+// console.log(basicOp('+', 4, 7)); // 11
+// console.log(basicOp('-', 15, 18)); // -3
+// console.log(basicOp('*', 5, 5)); // 25
+// console.log(basicOp('/', 49, 7)); // 7
