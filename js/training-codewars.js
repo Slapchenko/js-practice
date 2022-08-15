d7f2c8edc6564589efa81bc186bf4053fb5691b7
Serhii Lapchenko <lapchenko.ss@gmail.com>
2022-08-15 13:01:57 +0300

    Highest and Lowest

--- js/training-codewars.js
+++ js/training-codewars.js
@@ -255,3 +255,24 @@ The parameter of accum is a string which includes only letters from a..z and A..
 //     .split('')
 //     .reduce((acc, symbol, index) => acc + '-' + symbol + symbol.toLowerCase().repeat(index));
 // }
+
+// TODO Task #10 Highest and Lowest
+/* In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+Examples
+highAndLow("1 2 3 4 5");  // return "5 1"
+highAndLow("1 2 -3 4 5"); // return "5 -3"
+highAndLow("1 9 3 4 -5"); // return "9 -5"
+Notes
+All numbers are valid Int32, no need to validate them.
+There will always be at least one number in the input string.
+Output string must be two numbers separated by a single space, and highest number is first. */
+
+// function highAndLow(numbers) {
+//   const numbersArray = numbers.split(' ');
+//   const maxNumber = Math.max(...numbersArray);
+//   const minNumber = Math.min(...numbersArray);
+//   return `${maxNumber} ${minNumber}`;
+// }
+
+// console.log(highAndLow('8 3 -5 42 -1 0 0 -9 4 7 4 -4')); // '42 -9'
