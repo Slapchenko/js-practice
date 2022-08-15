458de9b1f68c8feb048c2af4d5df70c3e2b87cba
Serhii Lapchenko <lapchenko.ss@gmail.com>
2022-08-15 17:50:24 +0300

    completed task 17 - codewars

--- js/training-codewars.js
+++ js/training-codewars.js
@@ -418,11 +418,41 @@ Array can contain numbers or strings. X can be either.
 
 Return true if the array contains the value, false if not. */
 
-function check(a = [], x) {
-  return a.includes(x);
+// function check(a = [], x) {
+//   return a.includes(x);
+// }
+
+// console.log(check([66, 101], 66)); // true
+// console.log(check([101, 45, 75, 105, 99, 107], 107)); // true
+// console.log(check(['t', 'e', 's', 't'], 'e')); // true
+// console.log(check(['what', 'a', 'great', 'kata'], 'kat')); // false
+
+// TODO Task #17 Odd or Even?
+/* Task:
+Given a list of integers, determine whether the sum of its elements is odd or even.
+
+Give your answer as a string matching "odd" or "even".
+
+If the input array is empty consider it as: [0] (array with a zero).
+
+Examples:
+Input: [0]
+Output: "even"
+
+Input: [0, 1, 4]
+Output: "odd"
+
+Input: [0, -1, -5]
+Output: "even"
+Have fun! */
+
+function oddOrEven(array) {
+  return array.reduce((acc, number) => acc + number, 0) % 2 === 0 ? 'even' : 'odd';
 }
 
-console.log(check([66, 101], 66)); // true
-console.log(check([101, 45, 75, 105, 99, 107], 107)); // true
-console.log(check(['t', 'e', 's', 't'], 'e')); // true
-console.log(check(['what', 'a', 'great', 'kata'], 'kat')); // false
+console.log(oddOrEven([0])); // 'even'
+console.log(oddOrEven([1])); // 'odd'
+console.log(oddOrEven([])); // 'even'
+console.log(oddOrEven([0, 1, 5])); // 'even'
+console.log(oddOrEven([0, 1, 3])); // 'even'
+console.log(oddOrEven([1023, 1, 2])); // 'even'
