bc8c71a91626d91ebe13adbcec0341965e0605b9
Serhii Lapchenko <lapchenko.ss@gmail.com>
2022-08-16 16:43:29 +0300

    completed task 20 - codewars

--- js/training-codewars.js
+++ js/training-codewars.js
@@ -482,16 +482,33 @@ For example, if we run 9119 through the function, 811181 will come out, because 
 
 Note: The function accepts an integer and returns an integer */
 
-function squareDigits(num) {
-  return Number(
-    num
-      .toString()
-      .split('')
-      .map(number => Math.pow(number, 2))
-      .join('')
-  );
+// function squareDigits(num) {
+//   return Number(
+//     num
+//       .toString()
+//       .split('')
+//       .map(number => Math.pow(number, 2))
+//       .join('')
+//   );
+// }
+
+// console.log(squareDigits(3212)); // 9414
+// console.log(squareDigits(2112)); // 4114
+// console.log(squareDigits(0)); // 0
+
+// TODO Task #20 Binary Addition
+/* Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+
+The binary number returned should be a string.
+
+Examples:(Input1, Input2 --> Output (explanation)))
+
+1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary)
+5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary) */
+
+function addBinary(a, b) {
+  return (a + b).toString(2);
 }
 
-console.log(squareDigits(3212)); // 9414
-console.log(squareDigits(2112)); // 4114
-console.log(squareDigits(0)); // 0
+console.log(addBinary(1, 1)); // "10" (1 + 1 = 2 in decimal or 10 in binary)
+console.log(addBinary(5, 9)); // "1110" (5 + 9 = 14 in decimal or 1110 in binary)
