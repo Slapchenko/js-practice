93a20fb3bd6f9e74838ad73091c77e981fdd858e
Serhii Lapchenko <lapchenko.ss@gmail.com>
2022-08-16 22:54:08 +0300

    completed task 23 - codewars

--- js/training-codewars.js
+++ js/training-codewars.js
@@ -536,9 +536,92 @@ Zero (0) is not checked for any specific sign. Negative zeros make no mathematic
 
 (In this case, all triangles must have surface greater than 0 to be accepted). */
 
-function isTriangle(a, b, c) {
-  return a + b > c && b + c > a && c + a > b ? true : false;
+// function isTriangle(a, b, c) {
+//   return a + b > c && b + c > a && c + a > b ? true : false;
+// }
+
+// console.log(isTriangle(1, 2, 2)); // true
+// console.log(isTriangle(7, 2, 2)); // false
+
+// TODO Task #23
+/* Can you find the needle in the haystack?
+
+Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+After your function finds the needle it should return a message (as a string) that says:
+
+"found the needle at position " plus the index it found the needle, so:
+
+Example(Input --> Output)
+
+["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"] --> "found the needle at position 5" 
+Note: In COBOL, it should return "found the needle at position 6" */
+
+function findNeedle(haystack = []) {
+  return haystack.indexOf('needle') !== -1
+    ? `found the needle at position ${haystack.indexOf('needle')}`
+    : 'needle not found';
 }
 
-console.log(isTriangle(1, 2, 2)); // true
-console.log(isTriangle(7, 2, 2)); // false
+const haystack_1 = ['3', '123124234', undefined, 'needle', 'world', 'hay', 2, '3', true, false];
+const haystack_2 = [
+  '283497238987234',
+  'a dog',
+  'a cat',
+  'some random junk',
+  'a piece of hay',
+  'needle',
+  'something somebody lost a while ago',
+];
+const haystack_3 = [
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  8,
+  7,
+  5,
+  4,
+  3,
+  4,
+  5,
+  6,
+  67,
+  5,
+  5,
+  3,
+  3,
+  4,
+  2,
+  34,
+  234,
+  23,
+  4,
+  234,
+  324,
+  324,
+  'needle',
+  1,
+  2,
+  3,
+  4,
+  5,
+  5,
+  6,
+  5,
+  4,
+  32,
+  3,
+  45,
+  54,
+];
+const haystack_4 = ['3', '123124234', undefined, 'world', 'hay', 2, '3', true, false];
+
+console.log(findNeedle(haystack_1)); //'found the needle at position 3'
+console.log(findNeedle(haystack_2)); //'found the needle at position 5'
+console.log(findNeedle(haystack_3)); //'found the needle at position 30'
+console.log(findNeedle(haystack_4)); //'found the needle at position 30'
