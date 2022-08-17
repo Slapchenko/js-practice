1f896c133382024055a7dd41d9bfe77fec81789e
Serhii Lapchenko <lapchenko.ss@gmail.com>
2022-08-17 13:20:07 +0300

    completed task 31 - codewars

--- js/training-codewars.js
+++ js/training-codewars.js
@@ -746,13 +746,27 @@ Examples
 25  =>  true
 26  =>  false */
 
-var isSquare = function (n) {
-  return Number.isInteger(Math.sqrt(n)) ? true : false;
+// var isSquare = function (n) {
+//   return Number.isInteger(Math.sqrt(n)) ? true : false;
+// };
+
+// console.log(isSquare(-1)); // false
+// console.log(isSquare(0)); // true
+// console.log(isSquare(3)); // false
+// console.log(isSquare(4)); // true
+// console.log(isSquare(25)); // true
+// console.log(isSquare(26)); // false
+
+// TODO Task #31 Will you make it?
+/* You were camping with your friends far away from home, but when it's time to go back, you realize that your fuel is running out and the nearest pump is 50 miles away! You know that on average, your car runs on about 25 miles per gallon. There are 2 gallons left.
+
+Considering these factors, write a function that tells you if it is possible to get to the pump or not.
+
+Function should return true if it is possible and false if not. */
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+  return mpg * fuelLeft >= distanceToPump ? true : false;
 };
 
-console.log(isSquare(-1)); // false
-console.log(isSquare(0)); // true
-console.log(isSquare(3)); // false
-console.log(isSquare(4)); // true
-console.log(isSquare(25)); // true
-console.log(isSquare(26)); // false
+console.log(zeroFuel(50, 25, 2)); // true
+console.log(zeroFuel(100, 50, 1)); // false
