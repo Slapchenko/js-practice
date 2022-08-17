d318853511a942a4c3e999d1f7a9d6fe84d80b9a
Serhii Lapchenko <lapchenko.ss@gmail.com>
2022-08-17 15:19:21 +0300

    completed task 33 - codewars

--- js/training-codewars.js
+++ js/training-codewars.js
@@ -776,14 +776,42 @@ Function should return true if it is possible and false if not. */
 
 Example : n=5 --> [5,4,3,2,1] */
 
-const reverseSeq = n => {
-  let array = [];
+// const reverseSeq = n => {
+//   let array = [];
 
-  for (let i = 0; i < n; i += 1) {
-    array.unshift(i + 1);
-  }
+//   for (let i = 0; i < n; i += 1) {
+//     array.unshift(i + 1);
+//   }
 
-  return array;
-};
+//   return array;
+// };
 
-console.log(reverseSeq(5)); // [5, 4, 3, 2, 1]
+// console.log(reverseSeq(5)); // [5, 4, 3, 2, 1]
+
+// TODO Task #33 Total amount of points
+/* Our football team finished the championship. The result of each match look like "x:y". Results of all matches are recorded in the collection.
+
+For example: ["3:1", "2:2", "0:1", ...]
+
+Write a function that takes such collection and counts the points of our team in the championship. Rules for counting points for each match:
+
+if x > y: 3 points
+if x < y: 0 point
+if x = y: 1 point
+Notes:
+
+there are 10 matches in the championship
+0 <= x <= 4
+0 <= y <= 4 */
+
+function points(games = []) {
+  return games.reduce((acc, match) => {
+    if (match[0] > match[2]) return acc + 3;
+
+    if (match[0] < match[2]) return acc + 0;
+
+    if (match[0] === match[2]) return acc + 1;
+  }, 0);
+}
+
+console.log(points(['1:0', '2:0', '3:0', '4:0', '2:1', '3:1', '4:1', '3:2', '4:2', '4:3'])); // 30
