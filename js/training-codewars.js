2f37613370e251f7462c9a5b773677548407e2fc
Serhii Lapchenko <lapchenko.ss@gmail.com>
2022-11-01 14:49:43 +0000

    Update training-codewars.js

--- js/training-codewars.js
+++ js/training-codewars.js
@@ -862,11 +862,35 @@ filter_list([1,2,'a','b']) == [1,2]
 filter_list([1,'a','b',0,15]) == [1,0,15]
 filter_list([1,2,'aasf','1','123',123]) == [1,2,123] */
 
-function filter_list(l) {
-  return l.filter((element) => typeof element === "number");
-}
+// function filter_list(l) {
+//   return l.filter((element) => typeof element === "number");
+// }
 
-console.log(filter_list([1,2,'a','b'])); // [1,2]
+// console.log(filter_list([1,2,'a','b'])); // [1,2]
 
+// TODO Task #36 Remove the minimum
 
+/* The museum of incredible dull things
+The museum of incredible dull things wants to get rid of some exhibitions. Miriam, the interior architect, comes up with a plan to remove the most boring exhibitions. She gives them a rating, and then removes the one with the lowest rating.
+
+However, just as she finished rating all exhibitions, she's off to an important fair, so she asks you to write a program that tells her the ratings of the items after one removed the lowest one. Fair enough.
+
+Task
+Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with a lower index. If you get an empty array/list, return an empty array/list.
+
+Don't change the order of the elements that are left.
+
+Examples
+* Input: [1,2,3,4,5], output= [2,3,4,5]
+* Input: [5,3,2,1,4], output = [5,3,2,4]
+* Input: [2,2,1,2,1], output = [2,2,2,1] */
+
+// function removeSmallest(numbers) {
+//   return numbers.filter(
+//     (number, index, array) => index !== array.indexOf(Math.min(...array))
+//   );
+// }
+
+// console.log(removeSmallest([1, 2, 3, 4, 5])); // [2, 3, 4, 5]
+// console.log(removeSmallest([5, 3, 2, 1, 4])); // [5, 3, 2, 4]
 
